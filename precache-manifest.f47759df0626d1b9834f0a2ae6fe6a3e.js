d913324356922009af893f89e08b5d08283aea82
Destiny Jones <djones5713@gmail.com>
2019-08-12 08:26:54 -0400

    Updates

---
+++ precache-manifest.f47759df0626d1b9834f0a2ae6fe6a3e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bc0a7ed76e972edfb4acf78cee53768",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "a7def37ba3acdbed85e9",
+    "url": "/portfolio/static/css/main.0e32389d.chunk.css"
+  },
+  {
+    "revision": "a97135385101019baca1",
+    "url": "/portfolio/static/js/2.9ef77931.chunk.js"
+  },
+  {
+    "revision": "a7def37ba3acdbed85e9",
+    "url": "/portfolio/static/js/main.4086fff6.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "4aaae523b16286156911bbcdb5a99793",
+    "url": "/portfolio/static/media/Des.4aaae523.png"
+  },
+  {
+    "revision": "079e762a2a144fda0dc2325024ee20ec",
+    "url": "/portfolio/static/media/Name.079e762a.png"
+  },
+  {
+    "revision": "cd8f84938c9949b647940602733f919c",
+    "url": "/portfolio/static/media/devices.cd8f8493.png"
+  },
+  {
+    "revision": "3f3e0e29678f87ec76dcbe4ad66e6a86",
+    "url": "/portfolio/static/media/pos.3f3e0e29.png"
+  },
+  {
+    "revision": "16d6183f44ac3c21a3fd6186ff50d899",
+    "url": "/portfolio/static/media/profile.16d6183f.png"
+  },
+  {
+    "revision": "e25924b8fb39359dcc0329584cce5460",
+    "url": "/portfolio/static/media/yelp.e25924b8.png"
+  }
+]);
